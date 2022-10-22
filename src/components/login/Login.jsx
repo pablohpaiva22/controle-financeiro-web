af23b314baa8c2c2a898c10b1b898828007224f8
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-22 16:27:41 -0300

    feat: add new view Login

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styles from "./Login.module.scss";
+import useFetch from "../../hooks/useFetch";
+import { Link } from "react-router-dom";
+import Input from "../general/Input";
+
+function Login() {
+  const [user, setUser] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user, password }),
+    };
+
+    request("https://new-project-server.herokuapp.com/login", options);
+  }
+
+  React.useEffect(() => {
+    if (data && data.token) {
+      localStorage.setItem("token", data.token);
+    }
+  }, [data]);
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <h2>Login</h2>
+
+      <Input label="Usuário" id="user" type="text" setValue={setUser} value={user} />
+      <Input label="Senha" id="pass" type="password" setValue={setPassword} value={password} />
+
+      {loading ? (
+        <button className={styles.loading} disabled>
+          Carregando...
+        </button>
+      ) : (
+        <button>Entrar</button>
+      )}
+
+      {error && <p className={styles.error}>{error}</p>}
+
+      <p className={styles.footer}>
+        Ainda não é cadastrado?{" "}
+        <Link className={styles.footerLink} to={"cadastrar"}>
+          Cadastre-se
+        </Link>
+      </p>
+    </form>
+  );
+}
+
+export default Login;
