30d736baf81338d1ba51b2b321796fd595085e02
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-22 16:26:29 -0300

    feat: add hook useFetch

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    try {
+      setError(null);
+      setLoading(true);
+      const response = await fetch(url, options);
+      const json = await response.json();
+      if (!response.ok) throw new Error(json.msg);
+      setData(json);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return {
+    data,
+    loading,
+    error,
+    request,
+  };
+};
+
+export default useFetch;
