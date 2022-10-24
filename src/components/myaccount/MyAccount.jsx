86b014331cba789d8a5f85443a9d5a6c83407f47
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-24 19:35:47 -0300

    feat: add myaccount view

---
+++ src/components/myaccount/MyAccount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './MyAccount.module.scss'
+
+function MyAccount() {
+  return (
+    <div className={styles.container}>
+      <h1>My Account</h1>
+    </div>
+  )
+}
+
+export default MyAccount
