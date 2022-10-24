83f23dc5f7441c5dfe39d26ab69af572ede7797a
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-24 19:37:01 -0300

    feat: add header and new route myaccount

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from './components/login/Login'
 import SignUp from './components/signup/SignUp'
 import Home from './components/home/Home'
+import Header from './components/header/Header'
+import MyAccount from './components/myaccount/MyAccount'
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
+        <Header />
+        
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/cadastrar' element={<SignUp />} />
+            <Route path='/minhaconta' element={<MyAccount />} />
         </Routes>
       </BrowserRouter>
     </div>
