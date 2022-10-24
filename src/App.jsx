31e55739901cad0343c0d273cc7bdf8bc04adc05
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-24 14:20:09 -0300

    feat: add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import './App.scss'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Login from './components/login/Login'
+import SignUp from './components/signUp/SignUp'
+import Home from './components/home/Home'
+
 function App() {
 
   return (
-    <div>Hello World</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/cadastrar' element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
