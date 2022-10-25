831a571e48e42ed2145efcf5fa279631743b9223
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-25 14:01:54 -0300

    style: semantic HTML

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
       <h1>HOME</h1>
 
       <div className={styles.links}>
         <Link to='login'>Login</Link>
         <Link to='cadastrar'>SignUp</Link>
       </div>
-    </div>
+    </section>
   )
 }
 
