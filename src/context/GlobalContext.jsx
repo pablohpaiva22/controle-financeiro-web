56ad9341c0be7a6f707bed963cb061d1ee91fe77
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-25 12:47:03 -0300

    feat: change state if loggedin

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -5,6 +5,14 @@ export const GlobalContext = React.createContext();
 export function UserStorage({ children }) {
   const [login, setLogin] = React.useState(false);
 
+  React.useEffect(() => {
+    const user = localStorage.getItem('user')
+
+    if (user) {
+      setLogin(true)
+    }
+  }, [])
+
   return (
     <GlobalContext.Provider value={{ login, setLogin }}>
       {children}
