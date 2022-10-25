41ba950d3e8c3aec89197fffbfb4af682032e636
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-10-25 12:43:53 -0300

    feat: new nav field 'Minha Conta'

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { GlobalContext } from "../../context/GlobalContext";
 import styles from "./Header.module.scss";
 
 function Header() {
+  const { login, setLogin } = React.useContext(GlobalContext);
+  const navigate = useNavigate();
+
+  function handleClick(e) {
+    e.preventDefault();
+    localStorage.clear();
+
+    setLogin(false);
+    navigate("/login");
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -12,18 +24,38 @@ function Header() {
           </Link>
         </div>
 
-        <nav>
-          <div>
+        <nav className={styles.nav}>
+          {login ? (
+            <ul>
+              <li>
+                <NavLink
+                  to="/minhaconta"
+                  className={styles.link}
+                  style={({ isActive }) =>
+                    isActive ? { color: "rgb(255, 110, 43)" } : undefined
+                  }
+                >
+                  Minha Conta
+                </NavLink>
+              </li>
+              
+              <li>
+                <button className={styles.button} onClick={handleClick}>
+                  Sair
+                </button>
+              </li>
+            </ul>
+          ) : (
             <NavLink
               to="/login"
               className={styles.link}
               style={({ isActive }) =>
-                isActive ? {color: 'rgb(255, 110, 43)'} : undefined
+                isActive ? { color: "rgb(255, 110, 43)" } : undefined
               }
             >
               Entrar
             </NavLink>
-          </div>
+          )}
         </nav>
       </div>
     </header>
