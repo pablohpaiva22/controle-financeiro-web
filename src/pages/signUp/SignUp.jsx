48415b433b0ed4827d80ab524253b6eef50b7097
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-11-30 10:36:19 -0300

    feat: login after sign up

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./SignUp.module.scss";
 import useFetch from "../../hooks/useFetch"
+import useLogin from "../../hooks/useLogin"
 import Input from "../../components/general/Input";
 import { Link } from "react-router-dom";
 
@@ -10,7 +11,23 @@ function SignUp() {
   const [password, setPassword] = React.useState("");
   const [passCheck, setPassCheck] = React.useState("");
 
-  const { request, loading, error } = useFetch();
+  const { request, loading, error, data } = useFetch();
+  const { signIn, loginLoading } = useLogin()
+
+  React.useEffect(() => {
+    if (data) {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+
+      signIn('https://new-project-server.herokuapp.com/login', options)
+    }
+  }, [data])
+
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -42,7 +59,7 @@ function SignUp() {
       <Input id='password' type='password' value={password} setValue={setPassword} label='Senha' />
       <Input id='passCheck' type='password' value={passCheck} setValue={setPassCheck} label='Confirme a senha' />
 
-      {loading ? (
+      {loading || loginLoading ? (
         <button className={styles.loading} disabled>
           Carregando...
         </button>
