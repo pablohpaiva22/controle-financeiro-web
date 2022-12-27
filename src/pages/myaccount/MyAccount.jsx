9f677f14001eff0a8b631d51bc2af28d87fd5f12
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-12-27 14:16:48 -0300

    feat: myAccount page

--- src/pages/myaccount/MyAccount.jsx
+++ src/pages/myaccount/MyAccount.jsx
@@ -2,50 +2,65 @@ import React from "react";
 import styles from "./MyAccount.module.scss";
 
 function MyAccount() {
+  function handleClick() {
+    console.log('oi')
+  }
 
   return (
-    <div className={styles.container}>
-      <button className={`${styles.newItemButton} btn btn-warning`}>NOVO FILME</button>
+    <section className={styles.container}>
+      <div className={styles.dollarSignBg}>
+        <div className={styles.dollarSign}>
+          <img
+            src="../../src/assets/cifrao1.png"
+            alt="imagem de um cifrao verde"
+          />
+        </div>
+      </div>
 
-      <table className="table">
-        <thead className="thead-light">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Nota</th>
-            <th scope="col">Comentário</th>
-            <th scope="col">Data</th>
-            <th scope="col">Mais...</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton de</td>
-            <td>@fat</td>
-            <td>@fat</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      <span className={styles.welcomeMessage}>Bem-vindo, Pablo!!!</span>
+
+      <h1>CONTROLE FINANCEIRO</h1>
+
+      <div className={styles.balanceInfo}>
+        <div className={styles.balance}>
+          <span>SALDO</span>
+          <span>R$ 500,00</span>
+        </div>
+
+        <div className={styles.inputOutput}>
+          <div className={styles.input}>
+            <span>ENTRADAS</span>
+            <span>R$ 500,00</span>
+          </div>
+
+          <div className={styles.output}>
+            <span>SAÍDAS</span>
+            <span>R$ 500,00</span>
+          </div>
+        </div>
+      </div>
+
+      <button onClick={handleClick} className={styles.newTransactionBtn}>NOVA TRANSAÇÃO</button>
+
+      <div className={styles.transactionsContainer}>
+        <h2>TRANSAÇÕES</h2>
+
+        <div className={styles.transactions}>
+          <span className={styles.date}>09/10</span>
+          <p className={styles.description}>Corte de cabelo</p>
+          <span className={styles.price}>R$ 30,00</span>
+          <img
+            className={styles.deleteIcon}
+            src="../../src/assets/delete-icon.png"
+            alt="delete icon"
+          />
+        </div>
+
+        <div className={styles.pigImage}>
+          <img src="../../src/assets/pig.png" alt="cofrinho de porquinho" />
+        </div>
+      </div>
+    </section>
   );
 }
 
