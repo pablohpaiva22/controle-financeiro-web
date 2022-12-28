9d2aafff1f662100fcb2ce07d12082cb571f8c05
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-12-28 14:28:39 -0300

    refactor: change header link heferences

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,32 @@
 import React from "react";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
 import { GlobalContext } from "../../context/GlobalContext";
 import styles from "./Header.module.scss";
 
 function Header() {
   const { login, setLogin } = React.useContext(GlobalContext);
   const navigate = useNavigate();
+  const location = useLocation();
+
+  console.log(location.pathname);
 
   function handleClick(e) {
     e.preventDefault();
     localStorage.clear();
 
     setLogin(false);
-    navigate("/login");
+    navigate("/");
   }
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <Link to='/'>
-            <img src="../../src/assets/cifrao.png" alt="logo imagem de um cifrao" />
+          <Link to="/">
+            <img
+              src="../../src/assets/cifrao.png"
+              alt="logo imagem de um cifrao"
+            />
           </Link>
         </div>
 
@@ -32,29 +38,27 @@ function Header() {
                   to="/minhaconta"
                   className={styles.link}
                   style={({ isActive }) =>
-                    isActive ? { color: "#19880F" } : undefined
+                    isActive ? { color: "#4b6b9b" } : undefined
                   }
                 >
                   Pablo Paiva
                 </NavLink>
               </li>
-              
+
               <li>
                 <button className={styles.button} onClick={handleClick}>
                   Sair
                 </button>
               </li>
             </ul>
+          ) : location.pathname == "/cadastrar" ? (
+            <Link to="/" className={styles.link}>
+              Login
+            </Link>
           ) : (
-            <NavLink
-              to="/login"
-              className={styles.link}
-              style={({ isActive }) =>
-                isActive ? { color: "#19880F" } : undefined
-              }
-            >
-              Entrar
-            </NavLink>
+            <Link to="/cadastrar" className={styles.link}>
+              Sign-up
+            </Link>
           )}
         </nav>
       </div>
