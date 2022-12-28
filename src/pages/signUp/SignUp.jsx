3907bbe11b92ed74613e6385197c19a170d5beac
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-12-28 14:23:48 -0300

    style: change singup style

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./SignUp.module.scss";
-import useFetch from "../../hooks/useFetch"
-import useLogin from "../../hooks/useLogin"
+import useFetch from "../../hooks/useFetch";
+import useLogin from "../../hooks/useLogin";
 import Input from "../../components/general/Input";
 import { Link } from "react-router-dom";
 
@@ -12,7 +12,7 @@ function SignUp() {
   const [passCheck, setPassCheck] = React.useState("");
 
   const { request, loading, error, data } = useFetch();
-  const { signIn, loginLoading } = useLogin()
+  const { signIn, loginLoading } = useLogin();
 
   React.useEffect(() => {
     if (data) {
@@ -22,12 +22,11 @@ function SignUp() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ email, password }),
-      }
+      };
 
-      signIn('https://new-project-server.vercel.app/login', options)
+      signIn("https://new-project-server.vercel.app/login", options);
     }
-  }, [data])
-
+  }, [data]);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -45,37 +44,64 @@ function SignUp() {
       }),
     };
 
-    const url = "https://new-project-server.vercel.app/newuser"
+    const url = "https://new-project-server.vercel.app/newuser";
 
     request(url, options);
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.form}>
-      <h2>Criar Novo Usuário</h2>
+    <section className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h2>Criar Novo Usuário</h2>
 
-      <Input id='newUser' type='text' value={user} setValue={setUser} label='Usuário' />
-      <Input id='email' type='email' value={email} setValue={setEmail} label='E-mail' placehoder='email@example.com' />
-      <Input id='password' type='password' value={password} setValue={setPassword} label='Senha' />
-      <Input id='passCheck' type='password' value={passCheck} setValue={setPassCheck} label='Confirme a senha' />
+        <Input
+          id="newUser"
+          type="text"
+          value={user}
+          setValue={setUser}
+          label="Usuário"
+        />
+        <Input
+          id="email"
+          type="email"
+          value={email}
+          setValue={setEmail}
+          label="E-mail"
+          placehoder="email@example.com"
+        />
+        <Input
+          id="password"
+          type="password"
+          value={password}
+          setValue={setPassword}
+          label="Senha"
+        />
+        <Input
+          id="passCheck"
+          type="password"
+          value={passCheck}
+          setValue={setPassCheck}
+          label="Confirme a senha"
+        />
 
-      {loading || loginLoading ? (
-        <button className={styles.loading} disabled>
-          Carregando...
-        </button>
-      ) : (
-        <button>Criar</button>
-      )}
+        {loading || loginLoading ? (
+          <button className={styles.loading} disabled>
+            Carregando...
+          </button>
+        ) : (
+          <button>Criar</button>
+        )}
 
-      {error && <p className={styles.error}>{error}</p>}
+        {error && <p className={styles.error}>{error}</p>}
 
-      <p className={styles.footer}>
-        Já é cadastrado? Faça{" "}
-        <Link className={styles.footerLink} to={"/login"}>
-          Login
-        </Link>{" "}
-      </p>
-    </form>
+        <p className={styles.footer}>
+          Já é cadastrado? Faça{" "}
+          <Link className={styles.footerLink} to={"/login"}>
+            Login
+          </Link>{" "}
+        </p>
+      </form>
+    </section>
   );
 }
 
