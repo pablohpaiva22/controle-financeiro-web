201bab5442002967b3e0cc8dd23c2ab9fbd1ec43
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-12-29 15:22:20 -0300

    refactor: login after create new user logic

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import styles from "./SignUp.module.scss";
 import useFetch from "../../hooks/useFetch";
-import useLogin from "../../hooks/useLogin";
 import Input from "../../components/general/Input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { GlobalContext } from "../../context/GlobalContext";
 
 function SignUp() {
   const [user, setUser] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [passCheck, setPassCheck] = React.useState("");
-
   const { request, loading, error, data } = useFetch();
-  const { signIn, loginLoading } = useLogin();
-
-  React.useEffect(() => {
-    if (data) {
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      };
-
-      signIn("https://new-project-server.vercel.app/login", options);
-    }
-  }, [data]);
+  const { setLogin } = React.useContext(GlobalContext);
+  const navigate = useNavigate();
+  const {
+    data: loginData,
+    loading: loginLoading,
+    error: loginError,
+    request: loginRequest,
+  } = useFetch();
+  const {
+    data: userData,
+    loading: userLoading,
+    error: userError,
+    request: userRequest,
+  } = useFetch();
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -48,6 +46,49 @@ function SignUp() {
 
     request(url, options);
   }
+
+  React.useEffect(() => {
+    if (data) {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      };
+
+      loginRequest("https://new-project-server.vercel.app/login", options);
+    }
+  }, [data]);
+
+  React.useEffect(() => {
+    if (loginData && loginData.token) {
+      localStorage.setItem("token", loginData.token);
+
+      setLogin(true);
+
+      const url = "https://new-project-server.vercel.app/getuser";
+
+      const options = {
+        method: "GET",
+        headers: { authorization: "Bearer " + loginData.token },
+      };
+
+      userRequest(url, options);
+    }
+  }, [loginData, loginError]);
+
+  React.useEffect(() => {
+    if (userData) {
+      localStorage.setItem("user", JSON.stringify(userData));
+      navigate(`/minhaconta/${userData.id}`);
+    }
+    if (error) {
+      navigate("/");
+      localStorage.clear();
+      setLogin(false);
+    }
+  }, [userData, userError]);
 
   return (
     <section className={styles.container}>
@@ -84,7 +125,7 @@ function SignUp() {
           label="Confirme a senha"
         />
 
-        {loading || loginLoading ? (
+        {loading || loginLoading || userLoading ? (
           <button className={styles.loading} disabled>
             Carregando...
           </button>
