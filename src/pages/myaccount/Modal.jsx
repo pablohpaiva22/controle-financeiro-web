83721d9b5b37bf914768d39fc88506762d44164e
pablohpaiva22 <pablohpaiva22@gmail.com>
2023-01-12 12:20:22 -0300

    style: adjust modal on screen

--- src/pages/myaccount/Modal.jsx
+++ src/pages/myaccount/Modal.jsx
@@ -2,64 +2,72 @@ import React from "react";
 import styles from "./Modal.module.scss";
 import Input from "../../components/general/Input";
 
-function Modal() {
+function Modal({ setModal }) {
   const [description, setDescription] = React.useState("");
   const [price, setPrice] = React.useState("");
 
+  const handleClick = (event) => {
+    if (event.target.id === "modal") {
+      setModal(false)
+    }
+  };
+
   return (
-    <div className={styles.modal}>
-      <h2>NOVA TRANSAÇÃO</h2>
+    <div onClick={handleClick} className={styles.modalBg} id="modal">
+      <div className={styles.modal}>
+        <h2>NOVA TRANSAÇÃO</h2>
 
-      <form className={styles.newTransactionForm}>
-        <Input
-          label="DESCRIÇÃO"
-          type="text"
-          id="description"
-          value={description}
-          setValue={setDescription}
-          labelColor="white"
-        />
-        <Input
-          label="VALOR"
-          type="number"
-          id="price"
-          value={price}
-          setValue={setPrice}
-          labelColor="white"
-        />
+        <form className={styles.newTransactionForm}>
+          <Input
+            label="DESCRIÇÃO"
+            type="text"
+            id="description"
+            value={description}
+            setValue={setDescription}
+            labelColor="white"
+          />
+          <Input
+            label="VALOR"
+            type="number"
+            id="price"
+            value={price}
+            setValue={setPrice}
+            labelColor="white"
+          />
 
-        <div>
-          <span className={styles.transactionTypeLabel}>TIPO</span>
+          <div>
+            <span className={styles.transactionTypeLabel}>TIPO</span>
 
-          <div className={styles.typeBox}>
-            <div>
-              <input
-                type="radio"
-                id="typeInput"
-                name="type"
-                className={styles.typeInput}
-              />
-              <label htmlFor="typeInput" className={styles.typeLabel}>
-                Entrada
-              </label>
-            </div>
+            <div className={styles.typeBox}>
+              <div>
+                <input
+                  type="radio"
+                  id="typeInput"
+                  name="type"
+                  className={styles.typeInput}
+                />
+                <label htmlFor="typeInput" className={styles.typeLabel}>
+                  Entrada
+                </label>
+              </div>
 
-            <div>
-              <input
-                type="radio"
-                id="typeOutput"
-                className={styles.typeOutput}
-                name="type"
-              />
-              <label htmlFor="typeOutput" className={styles.typeLabel}>
-                Saída
-              </label>
+              <div>
+                <input
+                  type="radio"
+                  id="typeOutput"
+                  className={styles.typeOutput}
+                  name="type"
+                />
+                <label htmlFor="typeOutput" className={styles.typeLabel}>
+                  Saída
+                </label>
+              </div>
             </div>
           </div>
-        </div>
 
-        <button className={styles.modalButton}>CRIAR </button>
-      </form>
+          <button className={styles.modalButton}>CRIAR </button>
+        </form>
+      </div>
     </div>
   );
 }
