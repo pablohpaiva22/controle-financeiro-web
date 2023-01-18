4f0a248ef6c4dcfa665a74c53dea9485c3ccfc97
pablohpaiva22 <pablohpaiva22@gmail.com>
2023-01-18 15:11:53 -0300

    refactor: add delete transaction

--- src/pages/myaccount/Transactions.jsx
+++ src/pages/myaccount/Transactions.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import styles from './Transactions.module.scss'
+import useFetch from "../../hooks/useFetch";
+import styles from "./Transactions.module.scss";
 
-function Transactions({ data, loading }) {
+function Transactions({ data, loading, setUpdateTransactions }) {
   const [transactionsArray, setTransactionsArray] = React.useState([]);
+  const { data: deleteData, loading: deleteLoading, request } = useFetch();
 
   React.useEffect(() => {
     if (data) {
       setTransactionsArray(data.reverse());
     }
   }, [data]);
+
+  React.useEffect(() => {
+    if (deleteData) {
+      setUpdateTransactions((updateTransactions) => !updateTransactions)
+    }
+  }, [deleteData])
+
+  function handleClick(event) {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      const url = "http://127.0.0.1:3333/deletetransaction";
+
+      const options = {
+        method: "DELETE",
+        headers: {
+          authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({ id: event.target.id }),
+      };
+
+      request(url, options);
+    }
+  }
 
   return (
     <div className={styles.transactionsBox}>
@@ -17,7 +45,9 @@ function Transactions({ data, loading }) {
           return (
             <div key={item.id} className={styles.transactions}>
               <span className={styles.date}>{item.date}</span>
+
               <p className={styles.description}>{item.description}</p>
+
               <div className={styles.price}>
                 <span>R$ {item.price}</span>
                 <span
@@ -29,9 +59,24 @@ function Transactions({ data, loading }) {
                   className={styles.tag}
                 ></span>
               </div>
-              <div className={styles.deleteIcon}>
-                <img src="../../src/assets/delete-icon.png" alt="delete icon" />
-              </div>
+
+              {deleteLoading ? (
+                <button disabled onClick={handleClick} className={styles.deleteIcon}>
+                  <img
+                    id={item.id}
+                    src="../../src/assets/delete-icon.png"
+                    alt="delete icon"
+                  />
+                </button>
+              ) : (
+                <button onClick={handleClick} className={styles.deleteIcon}>
+                  <img
+                    id={item.id}
+                    src="../../src/assets/delete-icon.png"
+                    alt="delete icon"
+                  />
+                </button>
+              )}
             </div>
           );
         })}
