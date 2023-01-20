fe12e86dec679088068760e6957ee84ee1d61550
pablohpaiva22 <pablohpaiva22@gmail.com>
2023-01-20 14:33:07 -0300

    refactor: remove useless import

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { GlobalContext } from "../../context/GlobalContext";
 import styles from "./Header.module.scss";
 
