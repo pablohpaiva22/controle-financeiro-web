bd33e6a2c41388432d997201326ff4f3e61a8a24
pablohpaiva22 <pablohpaiva22@gmail.com>
2023-01-20 14:16:13 -0300

    fix: change folder name trying to fix a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.scss";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/SignUp";
+import SignUp from "./pages/signUp/SignUp";
 import Header from "./components/header/Header";
 import MyAccount from "./pages/myaccount/MyAccount";
 import { UserStorage } from "./context/GlobalContext";
