3f4de04ec8ac8aa6de6142541cbb58be89d4bf9e
pablohpaiva22 <pablohpaiva22@gmail.com>
2023-02-07 13:43:09 -0300

    refactor: create endpoints api file

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+export const SIGNIN = (email, password) => {
+  const url = "https://new-project-server.vercel.app/login";
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({ email, password }),
+  };
+
+  return {
+    url,
+    options
+  }
+};
+
+export const GET_USER = (token) => {
+  const url = "https://new-project-server.vercel.app/getuser";
+
+  const options = {
+    method: "GET",
+    headers: { authorization: "Bearer " + token },
+  };
+
+  return {
+    url,
+    options
+  }
+}
